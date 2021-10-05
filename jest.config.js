25440e976e7f78200d5c508fd02d846413c550dd
Radisav Savkovic <rades12340@gmail.com>
2021-10-05 17:59:07 +0200

    adding github actions workflow

--- jest.config.js
+++ jest.config.js
@@ -8,4 +8,12 @@ module.exports = {
       tsconfig: "tsconfig.jest.json",
     },
   },
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
 };
